c4292b2758266356b8cd912c745e0ad8f100b646
Paolo <gianpaolo1967@gmail.com>
2017-09-13 13:34:48 +0300

    initial commit

---
+++ js/main.js
@@ -0,0 +1,12 @@
+var images = document.getElementsByClassName('img-wrapper');
+
+for (var i = 0; i < images.length; i++) {
+  images[i].onmouseenter = showImgInfo;
+  images[i].onmouseleave = hideImgInfo;
+}
+function showImgInfo() {
+    this.getElementsByTagName('div')[0].style.visibility = 'visible';
+  }
+function hideImgInfo() {
+    this.getElementsByTagName('div')[0].style.visibility = 'hidden';
+  }
